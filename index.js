f78b634fc8c5526cb9d01b4861e1f52c560257d2
Haoqun Jiang <haoqunjiang@gmail.com>
2020-02-07 22:24:44 +0800

    fix: setup `import/resolver` and `import/extensions`
    Fixes yarn 2 compatibility.

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 module.exports = {
   extends: [
     require.resolve('eslint-config-standard')
-  ]
+  ],
+  settings: {
+    'import/resolver': {
+      // https://github.com/benmosher/eslint-plugin-import/issues/1396
+      [require.resolve('eslint-import-resolver-node')]: {},
+      [require.resolve('eslint-import-resolver-webpack')]: {
+        config: require.resolve('@vue/cli-service/webpack.config.js'),
+      },
+    },
+    'import/extensions': [
+      '.js',
+      '.jsx',
+      '.mjs',
+      '.ts',
+      '.tsx',
+    ],
+  },
 }
