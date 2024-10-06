2d65f05d86c98a22217c8ed484e30c4216243dd8
Haoqun Jiang <haoqunjiang@gmail.com>
2024-10-06 23:20:02 +0800

    chore: update most dependencies

--- examples/create-vue-ts-allow-jsx-in-vue/src/router/index.ts
+++ examples/create-vue-ts-allow-jsx-in-vue/src/router/index.ts
@@ -15,7 +15,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: async () => await import('../views/AboutView.vue')
     }
   ]
 })
--- examples/create-vue-ts-allow-jsx-in-vue/src/stores/counter.ts
+++ examples/create-vue-ts-allow-jsx-in-vue/src/stores/counter.ts
@@ -9,7 +9,7 @@ export const useCounterStore = defineStore({
     doubleCount: (state) => state.counter * 2
   },
   actions: {
-    increment() {
+    increment () {
       this.counter++
     }
   }
